1d2ce05f4ef35d36f7b5c28dac9c517572dcf0db
Paul MarisOUMary <paul.marisoumary@gmail.com>
2023-08-18 17:21:49 +0200

    add expressions

--- moon/parser.py
+++ moon/parser.py
@@ -1,6 +1,6 @@
 import ply.yacc as yacc
 
-from lexer import tokens, build_lexer
+from moon import tokens, build_lexer
 
 precedence = (
 	('left', 'OR'),
@@ -31,7 +31,7 @@ def p_statement(p):
 	p[0] = p[1]
 
 def p_if_statement(p):
-	"""if_statement : IF literal suite"""
+	"""if_statement : IF boolean_literal suite"""
 	p[0] = ('if_statement', p[2], p[3])
 
 def p_suite(p):
@@ -39,21 +39,76 @@ def p_suite(p):
 	p[0] = p[3]
 
 def p_line(p):
-	"""line : assignment
-			| literal"""
+	"""line : variable_declaration_statement
+			| expression"""
 	p[0] = p[1]
 
-def p_assignment(p):
-	"""assignment : IDENTIFIER IS literal"""
-	p[0] = ('variable_assignation', p[1], p[3])
+def p_variable_declaration_statement(p):
+	"""variable_declaration_statement : IDENTIFIER IS expression"""
+	p[0] = ('variable_declaration_statement', p[1], p[3])
 
-def p_literal(p):
-	"""literal : INTEGER
-			   | FLOAT
-			   | STRING
-			   | BOOLEAN
-			   | NULL"""
+def p_literals(p):
+	"""literals : integer_literal
+			   | float_literal
+			   | string_literal
+			   | boolean_literal
+			   | null_literal"""
 	p[0] = p[1]
+
+def p_interger_literal(p):
+	"""integer_literal : INTEGER"""
+	p[0] = ('integer_literal', p[1])
+
+def p_float_literal(p):
+	"""float_literal : FLOAT"""
+	p[0] = ('float_literal', p[1])
+
+def p_string_literal(p):
+	"""string_literal : STRING"""
+	p[0] = ('string_literal', p[1])
+
+def p_boolean_literal(p):
+	"""boolean_literal : BOOLEAN"""
+	p[0] = ('boolean_literal', p[1])
+
+def p_null_literal(p):
+	"""null_literal : NULL"""
+	p[0] = ('null_literal', p[1])
+
+def p_expression(p):
+	"""expression : literals
+				  | comparison_expression
+				  | arithmetic_expression
+				  | logical_expression"""
+	p[0] = p[1]
+
+def p_comparison_expression(p):
+	"""comparison_expression : expression LT expression
+							 | expression LE expression
+							 | expression IS expression
+							 | expression ISNT expression
+							 | expression GT expression
+							 | expression GE expression"""
+	p[0] = ('comparison_expression', p[2], p[1], p[3])
+
+def p_arithmetic_expression(p):
+	"""arithmetic_expression : expression PLUS expression
+							 | expression MINUS expression
+							 | expression MULTIPLY expression
+							 | expression DIVIDE expression
+							 | expression MODULO expression
+							 | expression EXPONENT expression"""
+	p[0] = ('arithmetic_expression', p[2], p[1], p[3])
+
+def p_logical_expression(p):
+	"""logical_expression : expression AND expression
+						  | expression OR expression
+						  | NOT expression"""
+	# Add the appropriate action here, depending on the logical operator
+	if len(p) == 4:
+		p[0] = ('logical_expression', p[2], p[1], p[3])
+	else:
+		p[0] = ('logical_expression', p[1], p[2])
 
 def p_error(p):
 	if p:
@@ -90,10 +145,10 @@ def parse_code(code: str):
 if __name__ == '__main__':
 	code = """
 variable is 5
-if 1
-	if 2
+if true
+	if false
 		variable is 3
-		if 4
+		if true
 			variable is 5
 		variable is 6
 x is 5"""
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,109 +2,96 @@ import pytest
 
 from moon import parse_code
 
-#Test Arithmetic Expressions
+# Literals
 @pytest.mark.parametrize("input_code, expected_output", [
-    ("1 + 1", [('+', 1, 1)]),
-    ("2 - 1", [('-', 2, 1)]),
-    ("3 * 3", [('*', 3, 3)]),
-    ("4 / 2", [('/', 4, 2)]),
-    ("5 % 2", [('%', 5, 2)]),
-    ("2 ** 3", [('**', 2, 3)])
+    ("1", [('integer_literal', 1)]),
+    ("1.0", [('float_literal', 1.0)]),
+    ("\"Hello World\"", [('string_literal', "Hello World")]),
+    ("true", [('boolean_literal', 'true')]),
+    ("false", [('boolean_literal', 'false')]),
+    ("null", [('null_literal', 'null')]),
+])
+def test_literals(input_code, expected_output):
+    assert parse_code(input_code) == expected_output
+
+# Expressions
+
+# Arithmetic expressions
+@pytest.mark.parametrize("input_code, expected_output", [
+    ("1 + 1", [('arithmetic_expression', '+', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 - 1", [('arithmetic_expression', '-', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 * 1", [('arithmetic_expression', '*', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 / 1", [('arithmetic_expression', '/', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 % 1", [('arithmetic_expression', '%', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 ** 1", [('arithmetic_expression', '**', ('integer_literal', 1), ('integer_literal', 1))]),
+
 ])
 def test_arithmetic_expressions(input_code, expected_output):
+    assert parse_code(input_code) == expected_output
 
-    pc = parse_code(input_code)
-    print(pc, type(pc), type(expected_output), input_code)
-
-    assert pc == expected_output
-
-# Test Logical Expressions
+# Comparison expressions
 @pytest.mark.parametrize("input_code, expected_output", [
-    ("true and true", [('and', 'true', 'true')]),    # AND
-    ("false and false", [('and', 'false', 'false')]),
-    ("false and true", [('and', 'false', 'true')]),
-    ("true and false", [('and', 'true', 'false')]),
-    ("true or true", [('or', 'true', 'true')]),     # OR
-    ("false or false", [('or', 'false', 'false')]),
-    ("false or true", [('or', 'false', 'true')]),
-    ("true or false", [('or', 'true', 'false')]),
-    ("not false", [('not', 'false')]),        # NOT
-    ("not true", [('not', 'true')]),
+    ("1 < 1", [('comparison_expression', '<', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 <= 1", [('comparison_expression', '<=', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 > 1", [('comparison_expression', '>', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 >= 1", [('comparison_expression', '>=', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 is 1", [('comparison_expression', 'is', ('integer_literal', 1), ('integer_literal', 1))]),
+    ("1 isnt 1", [('comparison_expression', 'isnt', ('integer_literal', 1), ('integer_literal', 1))]),
+])
+def test_comparison_expressions(input_code, expected_output):
+    assert parse_code(input_code) == expected_output
+
+# Logical expressions
+@pytest.mark.parametrize("input_code, expected_output", [
+    ("true and true", [('logical_expression', 'and', ('boolean_literal', 'true'), ('boolean_literal', 'true'))]),
+    ("true or true", [('logical_expression', 'or', ('boolean_literal', 'true'), ('boolean_literal', 'true'))]),
+    ("not true", [('logical_expression', 'not', ('boolean_literal', 'true'))]),
 ])
 def test_logical_expressions(input_code, expected_output):
-    pc = parse_code(input_code)
-
-    print(pc, type(pc), type(expected_output), input_code)
-
     assert parse_code(input_code) == expected_output
 
-# Test Relational Expressions
+# Unary expressions
+"""MISSING"""
+
+# Grouping expressions
+"""MISSING"""
+
+# Statements
+
+# Variable declaration statements
 @pytest.mark.parametrize("input_code, expected_output", [
-    ("1 < 2", [('<', 1, 2)]),
-    ("2 <= 2", [('<=', 2, 2)]),
-    ("3 > 1", [('>', 3, 1)]),
-    ("4 >= 5", [('>=', 4, 5)]),
-    ("5 is 5", [('is', 5, 5)]),
-    ("6 isnt 6", [('isnt', 6, 6)]),
+    ("x is 1", [('variable_declaration_statement', 'x', ('integer_literal', 1))]),
 ])
-def test_relational_expressions(input_code, expected_output):
+def test_variable_declaration_statements(input_code, expected_output):
     assert parse_code(input_code) == expected_output
 
-# Variables Declaration
+# If statements
 @pytest.mark.parametrize("input_code, expected_output", [
-    ("x is 10", [('variable_declaration', 'x', 10)]),
-    ("""u is true
-v is false
-w is null
-x is 5
-y is 0.55
-z is "Hello World"
-""", [
-    ('variable_declaration', 'u', 'true'),
-    ('variable_declaration', 'v', 'false'),
-    ('variable_declaration', 'w', 'null'),
-    ('variable_declaration', 'x', 5),
-    ('variable_declaration', 'y', 0.55),
-    ('variable_declaration', 'z', "Hello World"),
+    ("if true\n\t1", [('if_statement', ('boolean_literal', 'true'), [('integer_literal', 1)])]),
+    ("if true\n\tif false\n\t\t1", [
+        ('if_statement', ('boolean_literal', 'true'), [
+            ('if_statement', ('boolean_literal', 'false'), [
+                ('integer_literal', 1)
+            ])
+        ])
+    ]),
+    ("if true\n\tif false\n\t\tvariable is 3\n\t\tif true\n\t\t\tvariable is 5\n\t\tvariable is 6", [
+        ('if_statement', ('boolean_literal', 'true'), [
+            ('if_statement', ('boolean_literal', 'false'), [
+                ('variable_declaration_statement', 'variable', ('integer_literal', 3)),
+                ('if_statement', ('boolean_literal', 'true'), [
+                    ('variable_declaration_statement', 'variable', ('integer_literal', 5))
+                ]),
+                ('variable_declaration_statement', 'variable', ('integer_literal', 6))
+            ])
+        ])
     ]),
 ])
-def test_variables_declaration(input_code, expected_output):
+def test_if_statements(input_code, expected_output):
+    assert parse_code(input_code) == expected_output
 
-    result = parse_code(input_code)
-    print(result)
-
-    assert result == expected_output
-
-# Variable Declaration with Arithmetic Expressions
+# While statements
 @pytest.mark.parametrize("input_code, expected_output", [
-    ("x is 1 + 1", [('variable_declaration', 'x', ('+', 1, 1))]),
-    ("x is 2 - 1", [('variable_declaration', 'x', ('-', 2, 1))]),
-    ("x is 3 * 3", [('variable_declaration', 'x', ('*', 3, 3))]),
-    ("x is 4 / 2", [('variable_declaration', 'x', ('/', 4, 2))]),
-    ("x is 5 % 2", [('variable_declaration', 'x', ('%', 5, 2))]),
-    ("x is 2 ** 3", [('variable_declaration', 'x', ('**', 2, 3))]),
 ])
-def test_variable_declaration_with_arithmetic_expressions(input_code, expected_output):
-    pc = parse_code(input_code)
-    print(pc, type(pc), type(expected_output), input_code)
-
-    assert pc == expected_output
-
-# Variable Declaration with Logical Expressions
-@pytest.mark.parametrize("input_code, expected_output", [
-    ("x is true and true", [('variable_declaration', 'x', ('and', 'true', 'true'))]),
-    ("x is false and false", [('variable_declaration', 'x', ('and', 'false', 'false'))]),
-    ("x is false and true", [('variable_declaration', 'x', ('and', 'false', 'true'))]),
-    ("x is true and false", [('variable_declaration', 'x', ('and', 'true', 'false'))]),
-    ("x is true or true", [('variable_declaration', 'x', ('or', 'true', 'true'))]),
-    ("x is false or false", [('variable_declaration', 'x', ('or', 'false', 'false'))]),
-    ("x is false or true", [('variable_declaration', 'x', ('or', 'false', 'true'))]),
-    ("x is true or false", [('variable_declaration', 'x', ('or', 'true', 'false'))]),
-    ("x is not false", [('variable_declaration', 'x', ('not', 'false'))]),
-    ("x is not true", [('variable_declaration', 'x', ('not', 'true'))]),
-])
-def test_variable_declaration_with_logical_expressions(input_code, expected_output):
-    pc = parse_code(input_code)
-    print(pc, type(pc), type(expected_output), input_code)
-
-    assert pc == expected_output
+def test_while_statements(input_code, expected_output):
+    assert parse_code(input_code) == expected_output
