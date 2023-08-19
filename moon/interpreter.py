870b80d1e12fbd4f6e524a8a8f5c3b575733964b
Paul MarisOUMary <paul.marisoumary@gmail.com>
2023-08-19 23:47:26 +0200

    cleanup code

--- moon/interpreter.py
+++ moon/interpreter.py
@@ -10,43 +10,31 @@ def evaluate_expression(exp, environment):
 		return args[0]
 	elif type == "null_literal":
 		return None
-	elif type == "arithmetic_expression":
+	elif type == "logical_expression" and args[0] == "not":
+		return not evaluate_or_get_value(args[1], environment)
+	elif type in ("arithmetic_expression", "comparison_expression", "logical_expression"):
 		operator, left, right = args
-		left_val = evaluate_expression(left, environment) if isinstance(left, tuple) else environment[left]
-		right_val = evaluate_expression(right, environment) if isinstance(right, tuple) else environment[right]
+		left_val = evaluate_or_get_value(left, environment)
+		right_val = evaluate_or_get_value(right, environment)
 		return eval(f"{left_val} {operator} {right_val}")
-	elif type == "comparison_expression":
-		operator, left, right = args
-		left_val = evaluate_expression(left, environment)
-		right_val = evaluate_expression(right, environment)
-		return eval(f"{left_val} {operator} {right_val}")
-	elif type == "logical_expression":
-		if len(args) == 3:
-			operator, left, right = args
-			left_val = evaluate_expression(left, environment)
-			right_val = evaluate_expression(right, environment)
-			return eval(f"{left_val} {operator} {right_val}")
-		else:
-			operator, expr = args
-			return not evaluate_expression(expr, environment)
 	elif type == "variable_declaration_statement":
 		var_name, expression = args
 		value = evaluate_expression(expression, environment)
 		environment[var_name] = value
 		return value
 
+def evaluate_or_get_value(expression, environment):
+	if isinstance(expression, tuple):
+		if expression[0] == "call_statement":
+			return execute_statement(expression, environment, {})
+		return evaluate_expression(expression, environment)
+	return environment[expression]
+
 def execute_statement(statement, environment, actions):
 	type, *args = statement
 	if type == "variable_declaration_statement":
 		var_name, value = args
-		if isinstance(value, tuple) and value[0] == "ask_statement":
-			prompt = value[1][1]
-			value = input(prompt)
-		elif isinstance(value, tuple) and value[0] == "call_statement":
-			value = execute_statement(value, environment, actions)
-		else:
-			value = evaluate_expression(value, environment)
-		environment[var_name] = value
+		environment[var_name] = handle_variable_declaration(value, environment, actions)
 	elif type == "if_statement":
 		condition, suite = args
 		if evaluate_expression(condition, environment):
@@ -59,41 +47,36 @@ def execute_statement(statement, environment, actions):
 		name, params, suite = args
 		actions[name] = (params, suite)
 	elif type == "call_statement":
-		name, params = args
-		param_values = [evaluate_expression(p, environment) for p in params]
-		if name in actions:
-			func_params, func_body = actions[name]
-			local_env = dict(zip(func_params, param_values))
-			execute_statements(func_body, local_env, actions)
-			# Update the global variables with the local variables of the same name
-			for var_name, var_value in local_env.items():
-				if var_name in environment:
-					environment[var_name] = var_value
-			# Return the last expression result of the function
-			return local_env.get("return_value", None)
+		return handle_call_statement(args, environment, actions)
 	elif type == "return_statement":
 		values = [evaluate_expression(exp, environment) for exp in args[0]]
 		return values if len(values) != 1 else values[0]
 	elif type == "print_statement":
-		expressions = args[0]
-		values = []
-		for exp in expressions:
-			if isinstance(exp, tuple) and exp[0] == "call_statement":
-				value = execute_statement(exp, environment, actions)
-				values.append(value)
-			else:
-				value = evaluate_expression(exp, environment) if isinstance(exp, tuple) else environment[exp]
-				values.append(value)
-		print(*values)
+		print(*[evaluate_or_get_value(exp, environment) for exp in args[0]])
 	elif type == "ask_statement":
-		# Handle standalone ask statement
-		prompt = args[0][1] # Extract the prompt string
-		input(prompt)
+		input(args[0][1])
+
+def handle_variable_declaration(value, environment, actions):
+	if isinstance(value, tuple):
+		if value[0] == "ask_statement":
+			return input(value[1][1])
+		if value[0] == "call_statement":
+			return execute_statement(value, environment, actions)
+	return evaluate_expression(value, environment)
+
+def handle_call_statement(args, environment, actions):
+	name, params = args
+	param_values = [evaluate_expression(p, environment) for p in params]
+	if name in actions:
+		func_params, func_body = actions[name]
+		local_env = dict(zip(func_params, param_values))
+		execute_statements(func_body, local_env, actions)
+		environment.update({k: v for k, v in local_env.items() if k in environment})
+		return local_env.get("return_value", None)
 
 def execute_statements(statements, environment, actions):
 	for statement in statements:
 		result = execute_statement(statement, environment, actions)
-		# Check if it's a return statement and store the value
 		if statement[0] == "return_statement":
 			environment["return_value"] = result
 
