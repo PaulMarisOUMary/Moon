34d56dab881bae43a326f106cb0e5712f10e7674
Paul MarisOUMary <paul.marisoumary@gmail.com>
2023-08-19 18:44:00 +0200

    add debug optional arg

--- moon/main.py
+++ moon/main.py
@@ -1,16 +1,23 @@
-import sys
+import argparse
+
+from pprint import pprint
 
 from moon.lexer import build_lexer, print_tokens
 from moon.parser import build_parser
 from moon.interpreter import execute_program
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 moon.py <filename>")
+    args_parser = argparse.ArgumentParser(description="Run the Moon script.")
+    args_parser.add_argument("filename", help="Path to the Moon script file.")
+    args_parser.add_argument("-d", "--debug", action="store_true", help="Enable debugging mode.")
+
+    args = args_parser.parse_args()
+
+    if args.filename is None:
+        print("Usage: python3 moon.py <filename> [-d]")
         exit(1)
 
-    filename = sys.argv[1]
-    with open(filename, "r") as f:
+    with open(args.filename, "r") as f:
         source_code = f.read()
 
     source_code = source_code.lstrip('\n')
@@ -18,12 +25,19 @@ def main():
     lexer = build_lexer()
     lexer.input(source_code)
 
-    # print_tokens(source_code)
+    if args.debug:
+        print("===== Tokens =====")
+        print_tokens(source_code)
+        print("==================")
 
     parser = build_parser()
     parsed_code = parser.parse(source_code+'\n', lexer=lexer)
 
-    # print(parsed_code)
+    if args.debug:
+        print("=====   AST   =====")
+        pprint(parsed_code)
+        print("==================")
+
 
     execute_program(parsed_code)
 
