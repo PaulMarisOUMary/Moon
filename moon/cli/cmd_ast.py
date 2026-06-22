b3e767d1b1302e1f64977b013e61c6c0b64a1161
PaulMarisOUMary <paul.marisoumary@gmail.com>
2026-06-23 01:20:26 +0200

    Add ruff options and run format

--- moon/cli/cmd_ast.py
+++ moon/cli/cmd_ast.py
@@ -7,9 +7,7 @@ def view_ast(
     file: FileArg,
     start: StartOpt = StartRule.file_input,
     compact: bool = Option(
-        False,
-        "-c",
-        help="Print stringified AST instead of a tree shape."
-    )
+        False, "-c", help="Print stringified AST instead of a tree shape."
+    ),
 ) -> None:
     echo(f"Parsing {file.name} to view AST (compact={compact})...")
--- moon/parser/parser.py
+++ moon/parser/parser.py
@@ -10,38 +10,34 @@ REL_PATH = Path(__file__).parent
 
 
 class MoonIndenter(Indenter):
-	NL_type = "_NEWLINE"
-	OPEN_PAREN_types = []
-	CLOSE_PAREN_types = []
-	INDENT_type = "_INDENT"
-	DEDENT_type = "_DEDENT"
-	tab_len = 4
+    NL_type = "_NEWLINE"
+    OPEN_PAREN_types = []
+    CLOSE_PAREN_types = []
+    INDENT_type = "_INDENT"
+    DEDENT_type = "_DEDENT"
+    tab_len = 4
 
 
 def build_parser(
-	grammar_filename: str = str(LARK_FILE),
-	rel_to: str = str(REL_PATH),
-	*args: Any,
-	**kwargs: Any,
-	) -> Lark:
-		return Lark.open(
-			grammar_filename=grammar_filename,
-			rel_to=rel_to,
-			parser="lalr",
-			postlex=MoonIndenter(),
-			start=["start", "file_input", "single_input", "eval_input"],
-			lexer="contextual",
-			*args,
-			**kwargs,
-		)
+    grammar_filename: str = str(LARK_FILE),
+    rel_to: str = str(REL_PATH),
+    *args: Any,
+    **kwargs: Any,
+) -> Lark:
+    return Lark.open(
+        grammar_filename=grammar_filename,
+        rel_to=rel_to,
+        parser="lalr",
+        postlex=MoonIndenter(),
+        start=["start", "file_input", "single_input", "eval_input"],
+        lexer="contextual",
+        *args,
+        **kwargs,
+    )
 
 
-def parse(
-	source: str,
-	start: str = "file_input",
-	**kwargs: Any
-	) -> Tree:
-	parser = build_parser()
-	if not source.endswith('\n'):
-		source += '\n'
-	return parser.parse(source, start=start, **kwargs)
+def parse(source: str, start: str = "file_input", **kwargs: Any) -> Tree:
+    parser = build_parser()
+    if not source.endswith("\n"):
+        source += "\n"
+    return parser.parse(source, start=start, **kwargs)
